efa8f7a298909261ff949b09884eb3980955fa20
Fabricyo Barreto <bass.barreto@gmail.com>
2021-10-09 10:06:48 -0300

    Screen/Header: add resources and it was made the Header

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import { Text } from 'react-native';
-import { Container } from './styles';
+import { StatusBar } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+import Logo from '../../assets/logo.svg';
+import theme from '../../theme';
+import { Container, Header, TotalCars } from './styles';
 
 export function Home() {
   return (
     <Container>
-      <Text>Home</Text>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor={theme.colors.header}
+      />
+      <Header>
+        <Logo width={RFValue(108)} height={RFValue(12)} />
+        <TotalCars>Total de 12 carros</TotalCars>
+      </Header>
     </Container>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,8 +1,23 @@
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center;
-  justify-content: center;
   background-color: ${({ theme }) => theme.colors.background_primary};
+`;
+
+export const Header = styled.View`
+  width: 100%;
+  height: 113px;
+  background-color: ${({ theme }) => theme.colors.header};
+  flex-direction: row;
+  align-items: flex-end;
+  justify-content: space-between;
+  padding: 28px 16px;
+`;
+
+export const TotalCars = styled.Text`
+  font-size: ${RFValue(15)}px;
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+  color: ${({ theme }) => theme.colors.text};
 `;
