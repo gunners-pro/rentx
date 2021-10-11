a48c9e3cb35684531e71ebf089253168439291b2
Fabricyo Barreto <bass.barreto@gmail.com>
2021-10-11 11:52:56 -0300

    screen: It was made splashscreen animation

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -6,12 +6,14 @@ import { Scheduling } from '../screens/Scheduling';
 import { SchedulingDetails } from '../screens/SchedulingDetails';
 import SchedulingComplete from '../screens/SchedulingComplete';
 import MySchedules from '../screens/MySchedules';
+import Splash from '../screens/Splash';
 
 const { Navigator, Screen } = createStackNavigator();
 
 export function StackRoutes() {
   return (
     <Navigator screenOptions={{ headerShown: false }}>
+      <Screen name="Splash" component={Splash} />
       <Screen name="Home" component={Home} />
       <Screen name="CarDetails" component={CarDetails} />
       <Screen name="Scheduling" component={Scheduling} />
---
+++ src/screens/Splash/index.tsx
@@ -0,0 +1,81 @@
+import React, { useEffect } from 'react';
+import { StatusBar } from 'react-native';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  interpolate,
+  Extrapolate,
+  runOnJS,
+} from 'react-native-reanimated';
+import { useNavigation } from '@react-navigation/native';
+import { useTheme } from 'styled-components';
+import BrandSvg from '../../assets/brand.svg';
+import LogoSvg from '../../assets/logo.svg';
+import { Container } from './styles';
+
+type NavigationProps = {
+  navigate: (screen: string) => void;
+};
+
+export default function Splash() {
+  const theme = useTheme();
+  const navigation = useNavigation<NavigationProps>();
+  const splashAnimation = useSharedValue(0);
+  const brandStyle = useAnimatedStyle(() => ({
+    opacity: interpolate(splashAnimation.value, [0, 50], [1, 0]),
+    transform: [
+      {
+        translateX: interpolate(
+          splashAnimation.value,
+          [0, 50],
+          [0, -50],
+          Extrapolate.CLAMP,
+        ),
+      },
+    ],
+  }));
+  const logoStyle = useAnimatedStyle(() => ({
+    opacity: interpolate(splashAnimation.value, [0, 25, 50], [0, 0.5, 1]),
+    transform: [
+      {
+        translateX: interpolate(
+          splashAnimation.value,
+          [0, 50],
+          [-50, 0],
+          Extrapolate.CLAMP,
+        ),
+      },
+    ],
+  }));
+
+  useEffect(() => {
+    function startApp() {
+      navigation.navigate('Home');
+    }
+
+    splashAnimation.value = withTiming(50, { duration: 2000 }, () => {
+      'worklet';
+
+      runOnJS(startApp)();
+    });
+  }, [splashAnimation, navigation]);
+
+  return (
+    <>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor={theme.colors.header}
+      />
+      <Container>
+        <Animated.View style={[brandStyle, { position: 'absolute' }]}>
+          <BrandSvg width={80} height={50} />
+        </Animated.View>
+
+        <Animated.View style={[logoStyle, { position: 'absolute' }]}>
+          <LogoSvg width={180} height={20} />
+        </Animated.View>
+      </Container>
+    </>
+  );
+}
