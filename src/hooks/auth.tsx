694fe261010e77e7f9b74f25d53741e9d216eb69
Fabricyo Barreto <bass.barreto@gmail.com>
2021-11-24 22:18:04 -0300

    hook/auth: It was made the persistent user data

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,17 +1,22 @@
-import React, { createContext, ReactNode, useContext, useState } from 'react';
+import React, {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
+import { database } from '../database';
 import { api } from '../services/api';
+import { User as ModelUser } from '../database/model/User';
 
 interface User {
   id: string;
+  user_id: string;
   email: string;
   name: string;
   driver_license: string;
   avatar: string;
-}
-
-interface AuthState {
   token: string;
-  user: User;
 }
 
 interface SignInCredentials {
@@ -31,16 +36,53 @@ interface AuthProviderProps {
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
-  const [data, setData] = useState<AuthState>({} as AuthState);
+  const [data, setData] = useState<User>({} as User);
 
   async function signIn({ email, password }: SignInCredentials) {
-    const response = await api.post('/sessions', { email, password });
-    api.defaults.headers.authorization = `Bearer ${response.data.token}`;
-    setData({ token: response.data.token, user: response.data.user });
+    try {
+      const response = await api.post('/sessions', {
+        email,
+        password,
+      });
+
+      const { user, token } = response.data;
+
+      api.defaults.headers.authorization = `Bearer ${token}`;
+
+      const userCollection = database.get<ModelUser>('users');
+      await database.write(async () => {
+        await userCollection.create(newUser => {
+          newUser.user_id = user.id;
+          newUser.name = user.name;
+          newUser.email = user.email;
+          newUser.driver_license = user.driver_license;
+          newUser.avatar = user.avatar;
+          newUser.token = token;
+        });
+      });
+
+      setData({ token, ...user });
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 
+  useEffect(() => {
+    async function loadUserData() {
+      const userCollection = database.get<ModelUser>('users');
+      const response = await userCollection.query().fetch();
+
+      if (response.length > 0) {
+        const userData = response[0]._raw as unknown as User;
+        api.defaults.headers.authorization = `Bearer ${userData.token}`;
+        setData(userData);
+      }
+    }
+
+    loadUserData();
+  }, []);
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn }}>
+    <AuthContext.Provider value={{ user: data, signIn }}>
       {children}
     </AuthContext.Provider>
   );
