90fb43990387e004a68f0ed11785953b852696d1
Fabricyo Barreto <bass.barreto@gmail.com>
2021-11-25 20:53:38 -0300

    screen/home: It was resolved a problem of memory leak

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -53,21 +53,31 @@ export function Home() {
     Animated.createAnimatedComponent<FlatListProps<CarProps>>(FlatList);
 
   useEffect(() => {
+    let isMounted = true;
+
     async function fetchCars() {
       try {
         const response = await api.get('/cars');
-        setCars(response.data);
+        if (isMounted) {
+          setCars(response.data);
+        }
       } catch {
         Alert.alert(
           'Ops',
           'Não foi possível carregar os dados., Tente novamente mais tarde.',
         );
       } finally {
-        setLoading(false);
+        if (isMounted) {
+          setLoading(false);
+        }
       }
     }
 
     fetchCars();
+
+    return () => {
+      isMounted = false;
+    };
   }, []);
 
   useEffect(() => {
