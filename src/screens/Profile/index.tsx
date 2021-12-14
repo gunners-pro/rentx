97b2ea26e273b96f69a894178a81194cc03adaa4
Fabricyo Barreto <fabriciobarreto@hotmail.com>
2021-12-14 07:34:30 -0300

    screen/Profile: It was made the inputs fields of profile

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,5 +1,11 @@
 import React, { useState } from 'react';
 import { useNavigation } from '@react-navigation/core';
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
+import {
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import { useTheme } from 'styled-components';
 import { BackButton } from '../../components/BackButton';
@@ -16,54 +22,108 @@ import {
   Options,
   Option,
   OptionTitle,
+  Section,
 } from './styles';
+import { Input } from '../../components/Input';
+import { InputPassword } from '../../components/InputPassword';
+import { useAuth } from '../../hooks/auth';
 
 export function Profile() {
   const theme = useTheme();
   const navigation = useNavigation();
   const [option, setOption] = useState<'dataEdit' | 'passwordEdit'>('dataEdit');
+  const { user } = useAuth();
 
   function handleSignOut() {}
 
   return (
-    <Container>
-      <Header>
-        <HeaderTop>
-          <BackButton
-            color={theme.colors.shape}
-            onPress={() => navigation.goBack()}
-          />
-          <HeaderTitle>Editar perfil</HeaderTitle>
-          <LogoutButton onPress={handleSignOut}>
-            <Feather name="power" size={24} color={theme.colors.shape} />
-          </LogoutButton>
-        </HeaderTop>
-        <PhotoContainer>
-          <Photo source={{ uri: 'https://github.com/gunners-pro.png' }} />
-          <PhotoButton onPress={() => {}}>
-            <Feather name="camera" size={24} color={theme.colors.shape} />
-          </PhotoButton>
-        </PhotoContainer>
-      </Header>
+    <KeyboardAvoidingView behavior="position" enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <Header>
+            <HeaderTop>
+              <BackButton
+                color={theme.colors.shape}
+                onPress={() => navigation.goBack()}
+              />
+              <HeaderTitle>Editar perfil</HeaderTitle>
+              <LogoutButton onPress={handleSignOut}>
+                <Feather name="power" size={24} color={theme.colors.shape} />
+              </LogoutButton>
+            </HeaderTop>
+            <PhotoContainer>
+              <Photo source={{ uri: 'https://github.com/gunners-pro.png' }} />
+              <PhotoButton onPress={() => {}}>
+                <Feather name="camera" size={24} color={theme.colors.shape} />
+              </PhotoButton>
+            </PhotoContainer>
+          </Header>
 
-      <Content>
-        <Options>
-          <Option
-            active={option === 'dataEdit'}
-            onPress={() => setOption('dataEdit')}
-          >
-            <OptionTitle active={option === 'dataEdit'}>Dados</OptionTitle>
-          </Option>
-          <Option active={option === 'passwordEdit'}>
-            <OptionTitle
-              active={option === 'passwordEdit'}
-              onPress={() => setOption('passwordEdit')}
-            >
-              Trocar senha
-            </OptionTitle>
-          </Option>
-        </Options>
-      </Content>
-    </Container>
+          <Content style={{ marginBottom: useBottomTabBarHeight() * 1.5 }}>
+            <Options>
+              <Option
+                active={option === 'dataEdit'}
+                onPress={() => setOption('dataEdit')}
+              >
+                <OptionTitle active={option === 'dataEdit'}>Dados</OptionTitle>
+              </Option>
+              <Option active={option === 'passwordEdit'}>
+                <OptionTitle
+                  active={option === 'passwordEdit'}
+                  onPress={() => setOption('passwordEdit')}
+                >
+                  Trocar senha
+                </OptionTitle>
+              </Option>
+            </Options>
+
+            {option === 'dataEdit' ? (
+              <Section>
+                <Input
+                  iconName="user"
+                  placeholder="Nome"
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  defaultValue={user.name}
+                />
+                <Input
+                  iconName="mail"
+                  autoCapitalize="none"
+                  editable={false}
+                  defaultValue={user.email}
+                />
+                <Input
+                  iconName="credit-card"
+                  placeholder="CNH"
+                  keyboardType="numeric"
+                  defaultValue={user.driver_license}
+                />
+              </Section>
+            ) : (
+              <Section>
+                <InputPassword
+                  iconName="lock"
+                  placeholder="Senha atual"
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                />
+                <InputPassword
+                  iconName="lock"
+                  placeholder="Nova senha"
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                />
+                <InputPassword
+                  iconName="lock"
+                  placeholder="Repetir nova senha"
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                />
+              </Section>
+            )}
+          </Content>
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
