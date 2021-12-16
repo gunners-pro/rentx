2ce12c74665ba29fe2dd62db9e73e7363e32d930
Fabricyo Barreto <fabriciobarreto@hotmail.com>
2021-12-15 21:24:43 -0300

    with erro

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -6,16 +6,20 @@ import {
   FlatList,
   FlatListProps,
   LayoutAnimation,
+  Button,
 } from 'react-native';
 import Animated from 'react-native-reanimated';
 import { useNavigation } from '@react-navigation/native';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { useTheme } from 'styled-components';
+import { synchronize } from '@nozbe/watermelondb/sync';
 import Logo from '../../assets/logo.svg';
 import { CardCar } from '../../components/CardCar';
 import { Container, Header, TotalCars } from './styles';
 import { api } from '../../services/api';
 import { LoadAnimation } from '../../components/LoadAnimation';
+import { database } from '../../database';
+import { Car as ModelCar } from '../../database/model/Car';
 
 type NavigationProps = {
   navigate: (screen: string, data?: CarProps) => void;
@@ -42,7 +46,7 @@ export interface CarProps {
 }
 
 export function Home() {
-  const [cars, setCars] = useState<CarProps[]>([]);
+  const [cars, setCars] = useState<ModelCar[]>([]);
   const [loading, setLoading] = useState(true);
   const navigation = useNavigation<NavigationProps>();
   const theme = useTheme();
@@ -50,16 +54,17 @@ export function Home() {
   const scrollY = useRef(new Animated.Value(0)).current;
 
   const CustomFlatlist =
-    Animated.createAnimatedComponent<FlatListProps<CarProps>>(FlatList);
+    Animated.createAnimatedComponent<FlatListProps<ModelCar>>(FlatList);
 
   useEffect(() => {
     let isMounted = true;
 
     async function fetchCars() {
       try {
-        const response = await api.get('/cars');
+        const carCollection = database.get<ModelCar>('cars');
+        const carResponse = await carCollection.query().fetch();
         if (isMounted) {
-          setCars(response.data);
+          setCars(carResponse);
         }
       } catch {
         Alert.alert(
@@ -84,6 +89,25 @@ export function Home() {
     BackHandler.addEventListener('hardwareBackPress', () => true);
   }, []);
 
+  async function offlineSynchronize() {
+    await synchronize({
+      database,
+      pullChanges: async ({ lastPulledAt }) => {
+        const response = await api.get(
+          `cars/sync/pull?lastPulledVersion=${lastPulledAt || 0}`,
+        );
+
+        const { changes, latestVersion } = response.data;
+
+        return { changes, timestamp: latestVersion };
+      },
+      pushChanges: async ({ changes }) => {
+        const user = changes.users;
+        await api.post('/users/sync', user);
+      },
+    });
+  }
+
   return (
     <Container>
       <StatusBar
@@ -94,6 +118,8 @@ export function Home() {
         <Logo width={RFValue(108)} height={RFValue(12)} />
         <TotalCars>Total de {cars.length} carros</TotalCars>
       </Header>
+
+      <Button title="Sincronizar" onPress={offlineSynchronize} />
 
       {loading ? (
         <LoadAnimation />
